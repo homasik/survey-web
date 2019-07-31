05431e67c9de4f5b8f78f3eeb5c7a739604b2ef5
TyraelS <oshynkarenko1503@gmail.com>
2019-07-31 18:26:57 +0300

    fixed new lines in src

--- src/themes/GlobalStyle.js
+++ src/themes/GlobalStyle.js
@@ -7,15 +7,13 @@ const GlobalStyle = createGlobalStyle`
 		font-family: 'Roboto Condensed';
 		font-weight: normal;
 		font-style: normal;
-		src:
-			url('${RobotoCondensedRegular}') format('truetype')
+		src: url('${RobotoCondensedRegular}') format('truetype')
 	}
   @font-face {
 		font-family: 'Roboto Condensed';
 		font-weight: bold;
 		font-style: normal;
-		src:
-			url('${RobotoCondensedBold}') format('truetype')
+		src: url('${RobotoCondensedBold}') format('truetype')
   }
   body {
   	font-family: 'Roboto Condensed', sans-serif;
