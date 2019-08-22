8bfee55ce407a3d9bfd58fa1d732114d8b2ff69d
Den <44174619+homasik@users.noreply.github.com>
2019-08-22 19:06:39 +0300

    Update CTAButton.js

--- src/components/CTAButton/CTAButton.js
+++ src/components/CTAButton/CTAButton.js
@@ -4,7 +4,6 @@ import Button from '../Button';
 const CTAButton = styled(Button)`
   background-color: ${props => props.theme.color6};
   border: 1px solid ${props => props.theme.color7};
-  border-radius: 10px;
   color: ${props => props.theme.color1};
 `;
 
