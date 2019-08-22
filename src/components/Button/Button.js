09f8324b125a0bedd8c5a880b3881a1a3ceafc0b
Den <44174619+homasik@users.noreply.github.com>
2019-08-22 18:33:20 +0300

    Update Button.js

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 
 const Button = styled.button`
   width: 100%;
+  font-size: ${props => props.theme.FontSize3}
   height: 50px;
   border: 1px solid ${props => props.theme.color5};
 `;
