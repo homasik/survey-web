a754f2a200d9300e35db8125208e81fb9da2dccc
Den <44174619+homasik@users.noreply.github.com>
2019-08-22 19:10:41 +0300

    Update Delete.js

--- src/components/Delete/Delete.js
+++ src/components/Delete/Delete.js
@@ -4,6 +4,7 @@ import Button from '../Button';
 const Delete = styled(Button)`
   color: ${props => props.theme.color11};
   background: ${props => props.theme.color1};
+  max-width: 50px;
 `;
 
 export default Delete;
