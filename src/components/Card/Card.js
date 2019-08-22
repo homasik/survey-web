86a00dde22fa0dc3a55b9bab0d405b3616068d66
Den <44174619+homasik@users.noreply.github.com>
2019-08-22 19:05:45 +0300

    Update Card.js

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -5,6 +5,7 @@ const Card = styled.div`
   ${grid({})};
   border: 1px solid ${props => props.theme.color5};
   box-shadow: 1px 1px 5px ${props => props.theme.color5};
+  border-radius: ${props => props.theme.borderRadius2}
 `;
 
 export default Card;
