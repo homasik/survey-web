c740ee128a6c3d950a329e49fd264c9f55766cb9
Den <44174619+homasik@users.noreply.github.com>
2019-08-22 18:29:01 +0300

    Update Edit.js

--- src/components/Edit/Edit.js
+++ src/components/Edit/Edit.js
@@ -2,7 +2,10 @@ import styled from 'styled-components';
 import Delete from '../Button';
 
 const Edit = styled(Button)`
-  color: ${props => props.theme.color4};
+  width: 50px;
+  color: ${props => props.theme.color11};
+  background: ${props => props.theme.color1};
+  border: none;
 `;
 
 export default Edit;
